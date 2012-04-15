802691160710fdf5cada1c8270fb65e07a44f4c0
kusa <ondrej.tyrychtr@gmail.com>
2012-04-15 23:51:21 +0200

    h

--- test2.py
+++ test2.py
@@ -2,3 +2,11 @@ list = [1, 2, 3, 4]
 
 v, *k = list
 print(k)
+
+x=12379
+
+f=1234
+
+aa=1234
+
+ass
--- text.py
+++ text.py
@@ -1,1 +1,19 @@
-print("AAA")
+print("Zadavejte cisla \n")
+
+cislo = 0
+soucet = 0
+
+while True:
+    cislo = input("cislo: ")
+    if cislo:
+        try:
+            number = int(cislo)
+        except ValueError as err:
+            print(err)
+            continue
+        soucet += number
+    else:
+        break
+    
+print("Celkovy soucet: ", soucet)
+        s
