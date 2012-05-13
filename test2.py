3119f03b6d57bd7f691b5a41c300c99400e7bfc9
kusa <ondrej.tyrychtr@gmail.com>
2012-05-13 23:23:55 +0200

    d

--- test2.py
+++ test2.py
@@ -1,10 +1,75 @@
-list = [1, 2, 3, 4]
+#! /usr/bin/enc python3
 
-v, *k = list
-print(k)
 
-x="truse"
+zero = [
+       "  ***   ",
+       " *   *  ",
+       "*     * ",
+       "*     * ",
+       " *   *  ",
+       "  ***   "
+       ]
 
-u = "ANO" if x=="true" else "NE"
 
-print(u)
+one = [
+       "  *     ",
+       " **     ",
+       "* *     ",
+       "  *     ",
+       "  *     ",
+       "  *     "
+       ]
+
+two = [
+       " ***    ",
+       "*   *   ",
+       "   *    ",
+       "  *     ",
+       " *      ",
+       "*****   "
+       ]
+
+three = [
+       " ***    ",
+       "*   *   ",
+       "   *    ",
+       "   *    ",
+       "*   *   ",
+       " ***    "
+       ]
+
+numbers = [zero, one, two, three]
+seznam = []
+cd = []
+def napisCislo():
+    try:
+        
+        cislo = input("Zadejte cisla 1 ... 3")
+        rada = 0    
+        
+        for vl in cislo:
+            c = int(vl)
+            
+            if c > 3 or c < 0:
+                print("Cislo musim byt mezi 0 a 3")
+                return
+            cd.append(numbers[int(vl)])
+        xx=0  
+        
+        while xx<6:
+            seznam.insert(xx,"")
+            for vl2 in cd:
+                
+                seznam.insert(xx, str(seznam[xx] + vl2[xx])) 
+            xx = xx+1
+        
+        xx=0
+        for vl in seznam:
+            print(vl)
+            xx = xx+1
+            if xx>5:
+                break  
+    except ValueError as err:
+        print(err)
+        
+napisCislo()
