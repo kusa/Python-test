652671f43a16fa7ca0ded86b17aefe6da073900e
kusa <ondrej.tyrychtr@gmail.com>
2012-09-14 08:39:52 +0200

    d

--- on.py
+++ on.py
@@ -1,2 +1,38 @@
-print("On")
-/**/
+#! /usr/bin/enc python3
+
+n = ("ahoj", 2, 45 , "pes", 84, "ahoj")
+print(n.index("ahoj"))
+
+print(n.count(2))
+
+b = ["ahoj", 2, 45 , "pes", 84, "ahoj"]
+b += ["vasek"];
+c = ["ahoj", 2, 45 , "pes", 84, "ahoj", "vasek"]
+
+
+d = [[1,2],[2,3]]
+print('Delka', len(d))
+for x,y in d:
+    print(x,y)
+    
+import collections   
+user = collections.namedtuple('user' , 'name passw gender')
+
+co = user('Jan',1234,'male')
+print(co.name)
+
+se = [1,2,3,4,5]
+print(se[-1:])
+print(se.pop(1))
+print(se)
+
+jm = 'Moje jmeno je Ondra Tyrychtr'
+
+a, *b, c = jm.split()
+print(b)
+
+
+he = [1,2,3,4,5]
+
+for i in range(len(he)):
+    print(he[i])
