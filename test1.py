1071cc69e303bf7dbbdca943c8a503b6ee3c19fb
kusa <ondrej.tyrychtr@gmail.com>
2012-10-09 22:13:27 +0200

    kk

--- test1.py
+++ test1.py
@@ -1,15 +1,4 @@
 #!/usr/bin/python3.2
 
-s = [25, 45, 8, 12, 458, 453, 8]
-
-
-listx = [y for y in s if y>40]
-
-
-print(listx, 'oN')
-
-
-
-if 1 == 2:
-    print(1)
-
+def vrat():
+    return 'ondra'
--- text.py
+++ text.py
@@ -1,3 +1,16 @@
-print("AAAAAAAAAAAAAAAA")
+#!/usr/bin/python3.2
 
- 
+import collections 
+import test1
+
+
+dict = {'jmeno':'Ondra', 'Prijmeni': 'Borec', 'Age': 23}
+
+del dict['Prijmeni']
+
+for key,val in dict.items():
+    print(key, ' - ', val)
+    
+li  =[1,2]
+for v in li:
+    print(v)
