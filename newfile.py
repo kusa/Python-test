0eb6bf4fa7ae796dcc232b8271a76824f363e1f2
kusa <ondrej.tyrychtr@gmail.com>
2012-11-27 17:48:50 +0100

    Update newfile.py
    xxxxx

--- newfile.py
+++ newfile.py
@@ -4,7 +4,11 @@ import collections
 
 dict = {'jmeno':'Ondra', 'Prijmeni': 'Borec', 'Age': 23}
 
+jm = ['one', 'to'];
 del dict['Prijmeni']
 
 for key,val in dict.items():
     print(key, ' - ', val)
+    
+for vl in jm
+    print(vl)
