cf94c1c00865206edab124b2d6f8ebd86bf978bd
kusa <ondrej.tyrychtr@gmail.com>
2012-11-28 01:38:15 +0100

    ssssss

---
+++ cli.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3.2
+
+import sys
+import os
+import collections
+
+a = ['aa', 'cc', 'bb']
+a.reverse()
+print(a)
+
+b = {a:1}
+
+dict = dict(jmeno='Radim', bydliste="Nachod")
+print(sorted(dict))
